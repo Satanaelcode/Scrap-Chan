264907fd57a057b7eae48c4968fdf2cd3cd08ee7
EuGaming <121645064+Satanaelcode@users.noreply.github.com>
2023-02-10 09:21:31 +0100

    Add files via upload

---
+++ mainold.py
@@ -0,0 +1,35 @@
+import discord
+import string
+import random
+import time
+
+print('Type your token:')
+token = input()
+
+if token == '0':
+    token = 'ODAzMzc4Mzk0MDA2OTQ1ODk0.G5J7u_.Ld8N4WOsrInqkrpBgm73-89gq2kWcPJTv55_yQ'
+
+
+def random_string():
+    letters = string.ascii_lowercase
+    digits = string.digits
+    return ''.join(random.choice(letters) + random.choice(letters) + random.choice(digits) + random.choice(digits) + random.choice(digits) + random.choice(digits))
+
+
+intents = discord.Intents.all()
+client = discord.Client(command_prefix='!', intents=intents)
+
+
+@client.event
+async def on_ready():
+    while True:
+        massage = "https://prnt.sc/" + random_string() 
+        print('send link: ' + massage)
+        channel = client.get_channel(1071803218063003658)
+        # Send a message to the channel
+
+        await channel.send(massage)
+        time.sleep(25)
+
+
+client.run(token)
