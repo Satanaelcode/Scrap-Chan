9e6d6ef31b26aa7e533916550c348507bf9ace83
EuGaming <121645064+Satanaelcode@users.noreply.github.com>
2023-03-25 04:47:17 -0700

    MAIN UPDATE
    Added new scrap backend -EXPERIMENTAL

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-import discord
-import string
 import random
+import string
 import time
+
+import discord
 
 print('Type your token:')
 token = input()
 
 if token == '0':
-    token = '<DEFAULT TOKEN HERE>'
+    token = '<DEFAULT TOKEN>'
+
+## keep track of the index of the last word used
+last_word_index = -1
 
 
-def random_string():
+def random_string_srcsh():
     letters = string.ascii_lowercase
     digits = string.digits
-    return ''.join(random.choice(letters) + random.choice(letters) + random.choice(digits) + random.choice(digits) + random.choice(digits) + random.choice(digits))
-
+    return ''.join(
+        random.choice(letters) + random.choice(letters) + random.choice(digits) + random.choice(digits) + random.choice(
+            digits) + random.choice(digits))
 
 intents = discord.Intents.all()
 client = discord.Client(command_prefix='!', intents=intents)
@@ -22,14 +27,35 @@ client = discord.Client(command_prefix='!', intents=intents)
 
 @client.event
 async def on_ready():
+    global last_word_index
     while True:
-        massage = "https://prnt.sc/" + random_string() 
-        print('send link: ' + massage)
-        channel = client.get_channel(1071803218063003658)
-        # Send a message to the channel
 
-        await channel.send(massage)
-        time.sleep(25)
+        choice = random.randint(0, 1)
+        ## ignore that...
+        # print(str(choice) + ":debug.selected")
+        if choice == 1:
+            massage = "https://prnt.sc/" + random_string_srcsh()
+            print('send link: ' + massage)
+            channel1 = client.get_channel(1083729359988346932)
+            await channel1.send(str(massage), silent=True)
+            ## WORK IN PROGRESS
+            # channel2 = client.get_channel(1083729359988346932)
+            # await channel2.send(str(massage), silent=True)
+            time.sleep(25)
+        else:
+
+            with open('wordlist.txt') as f:
+                words = f.readlines()
+
+            ## Use the last_word_index to select the next word sequentially
+            last_word_index = (last_word_index + 1) % len(words)
+            selected_word = words[last_word_index].strip()
+
+            massage = "https://puu.sh/{}".format(selected_word)
+            print('send link: ' + massage)
+            channel1 = client.get_channel(1083729359988346932)
+            await channel1.send(str(massage), silent=True)
+            time.sleep(25)
 
 
 client.run(token)
